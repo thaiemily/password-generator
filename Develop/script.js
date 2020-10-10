32ffa0519f0f252f78a85140df323a6b086224b5
Pineapple Season <emsthai21@gmail.com>
2020-10-10 14:16:22 -0700

    prompt in criteria

--- Develop/script.js
+++ Develop/script.js
@@ -13,7 +13,26 @@ function writePassword() {
 
 }
 
+
+ // TO DO
+ // prompt: What is the length? >= 8 && <= 128
+ // confirm: character types 
+ //   lowercase
+ //   uppercase
+ //   numeric
+ //   special characters
+ //   generate a password 
 function generatePassword() {
+  var size = prompt("What is the length?");
+  console.log('size', size, typeof size);
+
+  var caseLetters = prompt("Do you want your password to have upper case or lower case letters?");
+  console.log('caseLetters', caseLetters, typeof caseLetters);
+  
+  var isUpperCase = confirm("Press okay to have upper case letters.");
+  console.log('isUpperCase', isUpperCase, typeof isUpperCase);
+
+
   return 0;
 }
 
